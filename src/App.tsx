8adb1af16694102c8e97c48a6b1c422fd045a342
Jack McBride <jack.mcbride@sihealth.co.uk>
2022-03-21 15:08:15 +0000

    Implemented view for playlists found from search

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { v4 } from "uuid";
 import { useEffect, useState } from "react";
 import { ReactComponent as SearchIcon } from "./images/search.svg";
+import Playlist from "./components/Playlist";
 interface Params {
   access_token?: string;
   token_type?: string;
@@ -45,8 +46,10 @@ function App() {
     undefined
   );
   const [searchTerm, setSearchTerm] = useState("");
+  const [searchedTerm, setSearchedTerm] = useState("");
   const [userPlaylists, setUserPlaylists] = useState([]);
   const [searchResults, setSearchResults] = useState([]);
+  const [showSecretSVG, setShowSecretSVG] = useState(false);
 
   useEffect(() => {
     const params = getHashParams();
@@ -193,6 +196,7 @@ function App() {
               placeholder="Search for songs or artists"
               onKeyDown={(e) => {
                 if (e.key === "Enter") {
+                  setSearchedTerm(searchTerm);
                   search();
                 }
               }}
@@ -203,10 +207,10 @@ function App() {
               <SearchIcon className="w-5 h-5 fill-black bg-green-600" />
             </button>
           </div>
-          {/* Implement displaying search results */}
           {searchResults.map((result) => (
-            <p className="text-white">{result.name}</p>
+            <Playlist playlist={result} searchedTerm={searchedTerm} />
           ))}
+          {}
           <button
             className="text-center p-4 border rounded-md"
             onClick={() => {
@@ -218,12 +222,194 @@ function App() {
           </button>
         </>
       ) : (
-        <button
-          className="text-center p-4 border rounded-md"
-          onClick={(e) => login(e)}
-        >
-          Login
-        </button>
+        <>
+          <svg className="w-72 h-72" viewBox="0 0 100 100">
+            <circle className="fill-green-600" cx="50" cy="50" r="40" />
+            <path
+              className="stroke-black hover:stroke-white"
+              d="M 31 62 Q 50 56, 67 66"
+              fill="transparent"
+              strokeLinecap="round"
+              strokeWidth="5"
+              // strokeDasharray=" 1 8"
+              strokeDashoffset="0"
+            />
+            <path
+              className="stroke-black hover:stroke-white"
+              d="M 29 49 Q 51 42, 72 54"
+              fill="transparent"
+              strokeLinecap="round"
+              strokeWidth="6"
+            />
+            <path
+              className="stroke-black hover:stroke-white"
+              d="M 26 36 Q 52 27 , 76 40"
+              fill="transparent"
+              strokeLinecap="round"
+              strokeWidth="7"
+            />
+          </svg>
+          <button
+            className="text-center p-4 border rounded-md"
+            onClick={(e) => login(e)}
+          >
+            Login
+          </button>
+          <svg className="w-72 h-72" viewBox="0 0 100 100">
+            <defs>
+              <linearGradient id="gradient" gradientTransform="rotate(90)">
+                <stop
+                  className="stop2 fill-green-600 text-green-600"
+                  offset="10%"
+                  stopColor="currentColor"
+                />
+                <stop
+                  className="stop2 text-green-200"
+                  offset="100%"
+                  stopColor="currentColor"
+                />
+              </linearGradient>
+              <radialGradient
+                id="radial"
+                cx="0.5"
+                cy="0.5"
+                fx="0.5"
+                fy="0.5"
+                r="0.1"
+                spreadMethod="reflect"
+              >
+                <stop
+                  className="stop2 fill-green-600 text-green-600"
+                  offset="0%"
+                  stopColor="currentColor"
+                />
+                <stop className="stop2" offset="100%" stopColor="white" />
+              </radialGradient>
+              <pattern
+                id="pattern"
+                x="0"
+                y="0"
+                width="10"
+                height="10"
+                patternUnits="userSpaceOnUse"
+              >
+                <rect x="0" y="0" width="10" height="10" fill="skyblue" />
+                <circle
+                  cx="5"
+                  cy="5"
+                  r="12"
+                  fill="url(#radial)"
+                  fillOpacity="0.5"
+                />
+                <rect
+                  x="0"
+                  y="0"
+                  width="5"
+                  height="5"
+                  fill="blue"
+                  fillOpacity="0.25"
+                />
+              </pattern>
+            </defs>
+            <circle fill="url(#pattern)" cx="50" cy="50" r="40" />
+            <path
+              d="M 25 35 A 3.5 3.5 0 0 0 30 40 Q 50 29, 73 42 A 3 3 0 0 0 77 37 Q 50 21  ,25 35 "
+              stroke="black"
+              fill="black"
+              strokeWidth="1"
+              strokeOpacity={0.7}
+            />
+
+            <path
+              d="M 26 53 A 3 3 0 0 0 30 57 Q 50 45 , 70 58 A 3 3 0 0 0 74 54 Q 50 37 , 26 53"
+              stroke="black"
+              fill="black"
+              strokeWidth="1"
+              strokeOpacity={0.7}
+            />
+
+            <path
+              d="M 30 67 A 2.5 2.5 0 0 0 33 71 Q 50 63, 67 73 A 2.5 2.5 0 0 0 70 69 Q 50 57, 30 67 "
+              stroke="black"
+              fill="black"
+              strokeWidth="1"
+              strokeOpacity={0.7}
+            />
+          </svg>
+          <button
+            className="ml-16 text-center text-black p-4 rounded-md cursor-default"
+            onClick={() => setShowSecretSVG(!showSecretSVG)}
+          >
+            Secret
+          </button>
+          <svg
+            className={"w-80 h-80 " + (showSecretSVG ? "" : "hidden")}
+            viewBox="0 0 100 100"
+          >
+            <circle className="fill-green-600" cx="50" cy="50" r="40" />
+            <rect
+              x="30"
+              y="30"
+              width="10"
+              height="10"
+              stroke="white"
+              fill="transparent"
+              strokeWidth="2"
+            />
+            <rect
+              x="60"
+              y="30"
+              rx="1"
+              ry="1"
+              width="10"
+              height="10"
+              stroke="white"
+              fill="transparent"
+              strokeWidth="2"
+            />
+            <ellipse
+              cx="50"
+              cy="65"
+              rx="20"
+              ry="5"
+              stroke="white"
+              fill="transparent"
+              strokeWidth="5"
+            />
+            <line
+              x1="10"
+              x2="90"
+              y1="15"
+              y2="15"
+              stroke="black"
+              strokeWidth="10"
+            />
+            <polyline
+              className="stroke-white"
+              points="20 80, 10 60, 10 35, 20 20, 80 20, 90 35, 90 60, 80 80"
+              fill="transparent"
+              strokeWidth="5"
+            />
+            <polygon
+              points="50 45 55 55 45 55"
+              stroke="black"
+              fill="transparent"
+              strokeWidth="1"
+            />
+            <path
+              d="M20,23 Q40,20 50,23 T90,23"
+              fill="none"
+              stroke="blue"
+              strokeWidth="5"
+            />
+            <path
+              d="M10 10 H 90 V 90 H 10 Z "
+              stroke="red"
+              fill="transparent"
+            />
+            <path d="M 10 10 C 20 20, 40 20, 50 10" stroke="purple" />
+          </svg>
+        </>
       )}
     </div>
   );
---
+++ src/components/Playlist.tsx
@@ -0,0 +1,65 @@
+import { useEffect, useRef, useState } from "react";
+
+export default function Playlist({ playlist, searchedTerm }) {
+  console.log(searchedTerm);
+  const ref = useRef(null);
+  const firstMatchRef = useRef(null);
+  const [firstMatchIndex, setFirstMatchIndex] = useState(null);
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.scrollBehaviour = "auto";
+      ref.current.scrollTop = 0;
+      const firstMatch = playlist.tracks.find(
+        (track) =>
+          track.name.toUpperCase().includes(searchedTerm.toUpperCase()) ||
+          track.artists.some((artist) =>
+            artist.name.toUpperCase().includes(searchedTerm.toUpperCase())
+          )
+      );
+      setFirstMatchIndex(playlist.tracks.indexOf(firstMatch));
+      setTimeout(() => {
+        ref.current.scrollBehaviour = "smooth";
+        ref.current.scrollTop =
+          firstMatchRef.current.offsetTop - ref.current.offsetTop;
+      }, 500);
+    }
+  }, [searchedTerm, playlist.tracks]);
+
+  useEffect(() => {}, []);
+
+  return (
+    <div className="m-2 p-2 md:w-full md: max-w-md justify-start w-72 border-gray-600 border-2 rounded-md">
+      <div className="flex space-x-4">
+        <img
+          className="h-20 w-20"
+          src={playlist?.images[1]?.url || playlist?.images[0]?.url}
+          // height={75}
+          // width={75}
+          alt="playlist"
+        ></img>
+        <div className="w-full">
+          <h1 className="text-lg">{playlist.name}</h1>
+          <p className="text-sm">{playlist.owner.display_name}</p>
+        </div>
+      </div>
+      <div
+        className="flex flex-col overflow-y-auto h-64 md:h-80"
+        ref={ref}
+        style={{ scrollBehavior: "smooth" }}
+      >
+        {playlist.tracks.map((track, index) => (
+          <p
+            className="border-b border-gray-300 p-2"
+            ref={index === firstMatchIndex ? firstMatchRef : null}
+          >
+            <span className="text-gray-200 font-light mr-2">{index + 1}</span>{" "}
+            {track.name} -{" "}
+            <span className="text-white font-light">
+              {track.artists.map((artist) => artist.name).join(", ")}
+            </span>
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+}

1409c8badbdd76a13dad9d3be88c6d225239bc03
Jack McBride <jack.mcbride@sihealth.co.uk>
2022-04-11 17:07:04 +0100

    Fixed the bug thanks to support from petyo

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import { motion } from "framer-motion";
 import LoginPage from "./components/LoginPage";
 import _ from "lodash";
 import LZstring from "lz-string";
-import SearchBar from "./components/SearchBar";
 import { Virtuoso } from "react-virtuoso";
+import Header from "./components/Header";
 interface Params {
   access_token?: string;
   token_type?: string;
@@ -498,66 +498,6 @@ function App() {
     setSearchResults(results);
   };
 
-  const Header = () => (
-    <div className="flex flex-col space-y-4 items-center">
-      <h1 className=" text-center text-xl lg:text-3xl font-semibold">
-        Spotify Playlist Search Tool
-      </h1>
-      <p>Hello {userProfile?.display_name.split(" ")[0]}</p>
-      <img
-        className="rounded-full"
-        src={userProfile?.images[0]?.url}
-        width={150}
-        height={150}
-        alt="User's spotify profile"
-      ></img>
-      {/* <SearchBar search={search} />
-      {searchResults.length > 0 && (
-        <motion.button
-          onClick={() =>
-            document.scrollingElement.scrollIntoView({
-              behavior: "smooth",
-            })
-          }
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-          className={
-            "rounded-full text-black text-xl bg-green-600 w-12 h-12 md:w-20 md:h-20  text-center fixed transition-all bottom-2 md:bottom-4 right-2 md:right-28 opacity-80 hover:opacity-100 focus:opacity-100 " +
-            (!showScrollToTop && "-bottom-24 md:-bottom-24")
-          }
-        >
-          <svg className={"w-12 h-12 md:w-20 md:h-20"} viewBox="0 0 100 100">
-            <polygon
-              points="47,75 47,50 38,50 50,28 62,50 53,50 53,75"
-              fill="black"
-              stroke="black"
-            />
-          </svg>
-        </motion.button>
-      )}
-      {errMsg && <p className="text-red-600">{errMsg}</p>}
-      {isLoadingPlaylists && (
-        <p>
-          Loading your playlists
-          <span className="w-2 inline-block">{dots}</span>{" "}
-          {userPlaylists.length} / {totalPlaylists} <br />
-          <span className="text-xs -mt-1">
-            (You can search just the loaded ones)
-          </span>
-        </p>
-      )}
-      {!isLoadingPlaylists && <p>Loaded {userPlaylists.length} playlists</p>}
-
-      {searchedTerm && (
-        <p>
-          {searchResults.length} matching out of {totalPlaylists} total
-          playlists
-        </p>
-      )}
-      {searchedTerm && searchResults.length === 0 && <p>No results found</p>} */}
-    </div>
-  );
-
   return (
     <div className="app flex flex-col items-center space-y-4 text-white w-full pb-4 relative h-full overflow-hidden">
       {accessToken ? (
@@ -568,6 +508,18 @@ function App() {
             totalCount={searchResults.length}
             useWindowScroll={true}
             overscan={20}
+            context={{
+              userProfile,
+              search,
+              searchResults,
+              showScrollToTop,
+              errMsg,
+              isLoadingPlaylists,
+              userPlaylists,
+              totalPlaylists,
+              dots,
+              searchedTerm,
+            }}
             components={{
               Header,
               Footer: () => (
@@ -596,14 +548,6 @@ function App() {
               );
             }}
           ></Virtuoso>
-
-          {/* {searchResults.map((result, index) => (
-            <Playlist
-              playlist={result}
-              searchedTerm={searchedTerm}
-              key={result?.id || index}
-            />
-          ))} */}
         </>
       ) : (
         <LoginPage login={login} />
---
+++ src/components/Header.tsx
@@ -0,0 +1,68 @@
+import SearchBar from "./SearchBar";
+import { motion } from "framer-motion";
+
+export default function Header({ context }) {
+  return (
+    <div className="flex flex-col space-y-4 items-center">
+      <h1 className=" text-center text-xl lg:text-3xl font-semibold">
+        Spotify Playlist Search Tool
+      </h1>
+      <p>Hello {context.userProfile?.display_name.split(" ")[0]}</p>
+      <img
+        className="rounded-full"
+        src={context.userProfile?.images[0]?.url}
+        width={150}
+        height={150}
+        alt="User's spotify profile"
+      ></img>
+      <SearchBar search={context.search} />
+      {context.searchResults.length > 0 && (
+        <motion.button
+          onClick={() =>
+            document.scrollingElement.scrollIntoView({
+              behavior: "smooth",
+            })
+          }
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className={
+            "rounded-full text-black text-xl bg-green-600 w-12 h-12 md:w-20 md:h-20  text-center fixed transition-all bottom-2 md:bottom-4 right-2 md:right-28 opacity-80 hover:opacity-100 focus:opacity-100 " +
+            (!context.showScrollToTop && "-bottom-24 md:-bottom-24")
+          }
+        >
+          <svg className={"w-12 h-12 md:w-20 md:h-20"} viewBox="0 0 100 100">
+            <polygon
+              points="47,75 47,50 38,50 50,28 62,50 53,50 53,75"
+              fill="black"
+              stroke="black"
+            />
+          </svg>
+        </motion.button>
+      )}
+      {context.errMsg && <p className="text-red-600">{context.errMsg}</p>}
+      {context.isLoadingPlaylists && (
+        <p>
+          Loading your playlists
+          <span className="w-2 inline-block">{context.dots}</span>{" "}
+          {context.userPlaylists.length} / {context.totalPlaylists} <br />
+          <span className="text-xs -mt-1">
+            (You can search just the loaded ones)
+          </span>
+        </p>
+      )}
+      {!context.isLoadingPlaylists && (
+        <p>Loaded {context.userPlaylists.length} playlists</p>
+      )}
+
+      {context.searchedTerm && (
+        <p>
+          {context.searchResults.length} matching out of{" "}
+          {context.totalPlaylists} total playlists
+        </p>
+      )}
+      {context.searchedTerm && context.searchResults.length === 0 && (
+        <p>No results found</p>
+      )}
+    </div>
+  );
+}
